03b1e5790feda1e02d3634b9d742848a68d4e323
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-01-23 15:06:51 -0800

    3.2.5

--- Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -6,9 +6,11 @@ public class RepeatBackground : MonoBehaviour
 {
     // Start is called before the first frame update
     private Vector3 startPos;
+    private float repeatWidth;
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
