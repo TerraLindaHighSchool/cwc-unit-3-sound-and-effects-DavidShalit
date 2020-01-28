56c65b5e8dde8d19648719891db7b6fca071d865
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-01-28 14:54:33 -0800

    Challenge 3 Continued

--- David Shalit - Personal Project/Assets/Scripts/PlayerController.cs
+++ David Shalit - Personal Project/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private float horizontalInput;
+    private float verticalInput;
     void Start()
     {
         
@@ -13,6 +15,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput / 5);
+        transform.Translate(Vector3.right * horizontalInput / 5);
     }
 }
